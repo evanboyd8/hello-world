f91c53a8ea563186ec19a3d6fd1ebee33431df3e
Evan Boyd <e.boyd918@gmail.com>
2023-05-26 14:29:53 -0500

    Finished Hello, World

--- hello_test.go
+++ hello_test.go
@@ -21,6 +21,12 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Evan", "French")
+		want := ("Bonjour, Evan")
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {

76790462394d0546cad686b173483ae6914ce237
Evan Boyd <e.boyd918@gmail.com>
2023-05-26 13:44:46 -0500

    the addition of spanish

--- hello.go
+++ hello.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Holla"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
